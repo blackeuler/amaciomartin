449862e5b1b573c6e230447acf41a42f61efecb0
Christopher Miller <miller1997@gmail.com>
2019-09-02 09:50:30 -0400

    Gallery fixed, hom fixed need to do item router problem

--- pages/album/[name].js
+++ pages/album/[name].js
@@ -3,44 +3,55 @@ import Page from '../../components/page';
 import Album from '../../components/album';
 import {client} from '../../services/contentfulClient'
 
-export default function Photos (entries) {
-  const router = useRouter();
-   entries = entries.entries
-  return (
-    <Page>
-      <h1>{router.query.name}</h1>
-      <div className="grid ">
-        {
-            entries.map(entry =>(
-                <Album
-                    title={`${entry.fields.title}`}
-                    cover={`${entry.fields.photo.fields.file.url}`}
-                />      
-            ))
+class Photos extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            entries:{}
         }
-      </div>
-        <style jsx>
-            {`
-                h1{
-                    text-align:center;
+    }
+    componentDidMount(){
+      const fetchData = async () =>{
+        var content = await client.getEntries({'sys.id':query.id})
+        var entries = await content.items[0].fields.images;
+        this.setState(entries)
+      }
+      fetchData();
+    }
+    render(){
+        const router = useRouter();
+        return (
+            <Page>
+              <h1>{router.query.name}</h1>
+              <div className="grid ">
+                {
+                    entries.map(entry =>(
+                        <Album
+                            title={`${entry.fields.title}`}
+                            cover={`${entry.fields.photo.fields.file.url}`}
+                        />      
+                    ))
                 }
+              </div>
+                <style jsx>
+                    {`
+                        h1{
+                            text-align:center;
+                        }
+        
+                        @media screen and (min-width: 600px) {
+                            .grid {
+                                display: flex;
+                                flex-wrap: wrap;
+                                flex-direction: row;
+                            }            
+                        }
+                    `}
+                </style>
+            </Page>
+          );
 
-                @media screen and (min-width: 600px) {
-                    .grid {
-                        display: flex;
-                        flex-wrap: wrap;
-                        flex-direction: row;
-                    }            
-                }
-            `}
-        </style>
-    </Page>
-  );
+    }
 }
 
-Photos.getInitialProps = async ({query})=> {
-    
-    var content = await client.getEntries({'sys.id':query.id})
-      var entries = await content.items[0].fields.images;
-      return {entries};
-    }
+export default Photos;
--- pages/gallery.js
+++ pages/gallery.js
@@ -10,65 +10,58 @@ var client = contentful.createClient({
   accessToken: 'W2aFltqZ34NA4Wb8OYtWCIIp6jOvwqxId7Hj9o85tS4'
 })
 
-
-const title = "Hello"
-const Gallery = (props) =>(
-    
-    <Page>
-        
-        <div className="grid ">
-
-        {
-          props.entries.map(entry =>(
-            <Link href={`/album/${entry.fields.title}`} as={`/album/${entry.fields.title}?id=${entry.sys.id}`}>
-                <a>
-                    <Album
-                    title={`${entry.fields.title}`}
-                    cover={`${entry.fields.coverImage.fields.file.url}`}
-                />
-                </a>
-            </Link>
-          ))
+class Gallery extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            entries:{}
         }
-           
-           
-           
-            
-            
-        </div>
-
-        <style jsx>
-            {`
-
-
-            @media screen and (min-width: 600px) {
-                .grid {
-                    display: flex;
-                    flex-wrap: wrap;
-                    flex-direction: row;
-                }            
-            }
-
-            
-            
-            `}
-        </style>
-    </Page>
-)
-
-Gallery.getInitialProps = async function() {
-    var contentTypeId = 'photoGallery'
-    
-    var content = await client.getEntries({
-        content_type: contentTypeId
-      });
-      var entries =content.items
-      
-      return {entries:entries};
     }
-    
-    
-    
-  
+    componentDidMount(){
+        const fetchData = async ()=>{
+            var contentTypeId = 'photoGallery'
+            var content = await client.getEntries({
+                content_type: contentTypeId
+            });
+            var entries =content.items
+            this.setState({
+                entries
+            })
+        }
+        fetchData();
+    }
 
+    render(){
+        return(
+            <Page>
+                <div className="grid ">
+                {
+                    this.state.entries[0] ?(
+                    this.state.entries.map(entry =>(
+                        <Link href={`/album/${entry.fields.title}`} as={`/album/${entry.fields.title}?id=${entry.sys.id}`}>
+                            <a>
+                                <Album
+                                title={`${entry.fields.title}`}
+                                cover={`${entry.fields.coverImage.fields.file.url}`}
+                                />
+                            </a>
+                        </Link>
+                    ))):(<div>Loading</div>)
+                } 
+                </div>
+                <style jsx>
+                    {`
+                        @media screen and (min-width: 600px) {
+                            .grid {
+                                display: flex;
+                                flex-wrap: wrap;
+                                flex-direction: row;
+                            }            
+                        }
+                    `}
+                </style>
+            </Page>
+        )
+    }
+}
 export default Gallery;
