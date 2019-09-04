9c2a710fba070b64c30920060ca661f44778f07e
Christopher Miller <miller1997@gmail.com>
2019-09-03 23:07:48 -0400

    Fixed gallery with props

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,66 +2,49 @@ import React from 'react';
 import Page from '../components/page';
 import Album from '../components/album';
 import Link from 'next/link';
-import * as contentful from 'contentful';
+import {client} from '../services/contentfulClient';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-var client = contentful.createClient({
-  space: '63i3zfsp3i6k',
-  accessToken: 'W2aFltqZ34NA4Wb8OYtWCIIp6jOvwqxId7Hj9o85tS4'
-})
 
-class Gallery extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            entries:{}
-        }
-    }
-    componentDidMount(){
-        const fetchData = async ()=>{
-            var contentTypeId = 'photoGallery'
-            var content = await client.getEntries({
-                content_type: contentTypeId
-            });
-            var entries =content.items
-            this.setState({
-                entries
-            })
-        }
-        fetchData();
-    }
-
-    render(){
-        return(
-            <Page>
-                <div className="grid ">
-                {
-                    this.state.entries[0] ?(
-                    this.state.entries.map(entry =>(
-                        <Link href={`/album/${entry.fields.title}`} as={`/album/${entry.fields.title}?id=${entry.sys.id}`}>
-                            <a>
-                                <Album
-                                title={`${entry.fields.title}`}
-                                cover={`${entry.fields.coverImage.fields.file.url}`}
-                                />
-                            </a>
-                        </Link>
-                    ))):(<div>Loading</div>)
-                } 
-                </div>
-                <style jsx>
-                    {`
-                        @media screen and (min-width: 600px) {
-                            .grid {
-                                display: flex;
-                                flex-wrap: wrap;
-                                flex-direction: row;
-                            }            
-                        }
-                    `}
-                </style>
-            </Page>
-        )
-    }
+function Gallery(entries){
+    return(
+        <Page>
+            <div className="grid ">
+            {
+                entries!=null ?(
+                entries.entries.map(entry =>(
+                    <Link href={`/album/${entry.fields.title}`} as={`/album/${entry.fields.title}?id=${entry.sys.id}`}>
+                        <a>
+                            <Album
+                            title={`${entry.fields.title}`}
+                            cover={`${entry.fields.coverImage.fields.file.url}`}
+                            />
+                        </a>
+                    </Link>
+                ))):(<div>Loading</div>)
+            } 
+            </div>
+            <style jsx>
+                {`
+                    @media screen and (min-width: 600px) {
+                        .grid {
+                            display: flex;
+                            flex-wrap: wrap;
+                            flex-direction: row;
+                        }            
+                    }
+                `}
+            </style>
+        </Page>
+    )
 }
+
+Gallery.getInitialProps = async () => {
+        var contentTypeId = 'photoGallery'
+        var content = await client.getEntries({
+            content_type: contentTypeId
+        });
+        var entries = await content.items
+        return ({entries:entries})
+  }
 export default Gallery;
