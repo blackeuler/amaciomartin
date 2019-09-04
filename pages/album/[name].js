d62a4515fb6c6117897b92bce8f3a3896dd4d85b
Christopher Miller <miller1997@gmail.com>
2019-09-04 02:10:38 -0400

    Fixed props on Id page

--- pages/album/[name].js
+++ pages/album/[name].js
@@ -1,36 +1,18 @@
-import { withRouter } from 'next/router';
 import Page from '../../components/page';
 import Album from '../../components/album';
 import {client} from '../../services/contentfulClient'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-class Photos extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            entries:{},
-            id:props.router.query.id
-        }
-    }
-    componentDidMount(){
-      const fetchData = async () =>{
-        console.log(this.state.id)
-        var content = await client.getEntries({'sys.id':this.state.id})
-        var entries = await content.items[0].fields.images;
-        console.log(entries)
-        this.setState({entries:entries})
-      }
-      fetchData();
-    }
-    render(){
-        return (
-            <Page>
+
+function Photos(entries){
+    return(
+        <Page>
               
               <div className="grid ">
                 {
-                    this.state.entries[0] ? (
-                    this.state.entries.map(entry =>(
+                    entries.entries[0] ? (
+                    entries.entries.map(entry =>(
                         <Album
                             title={`${entry.fields.title}`}
                             cover={`${entry.fields.photo.fields.file.url}`}
@@ -54,9 +36,15 @@ class Photos extends React.Component{
                     `}
                 </style>
             </Page>
-          );
-
-    }
+    )
 }
 
-export default withRouter(Photos);
+Photos.getInitialProps = async function({query}) {
+    var content = await client.getEntries({'sys.id':query.id})
+    var entries = await content.items[0].fields.images;
+    return {
+       entries:entries
+    };
+  };
+
+  export default Photos;
